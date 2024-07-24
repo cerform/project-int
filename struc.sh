ae90a67ff62276e7fba7fc5f47ebec4a9352b2f2
cerform <cruzerhell@gmail.com>
2024-07-24 18:33:55 +0300

    dc-3

---
+++ struc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir -p project-int-chart/templates
+touch project-int-chart/Chart.yaml
+touch project-int-chart/values.yaml
+touch project-int-chart/templates/nginx-deployment.yaml
+touch project-int-chart/templates/nginx-service.yaml
+touch project-int-chart/templates/nginx-ingress.yaml
+touch project-int-chart/templates/app-deployment.yaml
+touch project-int-chart/templates/hpa.yaml
+touch project-int-chart/templates/pvc.yaml
